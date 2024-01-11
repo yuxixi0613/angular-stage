b6c0462f0d8d048cc08bd8b7c89d65367a458297
喜羊羊 <446094999@qq.com>
2024-01-12 02:14:31 +0800

    ng-zorro

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,23 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { zh_CN, provideNzI18n } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import zh from '@angular/common/locales/zh';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+registerLocaleData(zh);
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimations()]
+  providers: [
+    provideRouter(routes),
+    provideAnimations(),
+    provideNzI18n(zh_CN),
+    importProvidersFrom(FormsModule),
+    importProvidersFrom(HttpClientModule),
+    provideAnimations()
+  ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,23 +1,27 @@
 import { Routes } from '@angular/router';
 import { SchoolManageComponent } from '@pages/school-manage/school-manage.component';
+import { LoginComponent } from './pages/login/login.component';
 
 // 一级路由
 export const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  {
+    path: 'login',
+    component: LoginComponent,
+    data: { preload: true },
+    loadChildren: () => import('./pages/login/login-routing')
+  },
   {
     path: 'school-manage',
     title: '校园管理',
     component: SchoolManageComponent
   }
-  // {
-  //   path: 'school-consume',
-  //   title: '校园消费',
-  // },
-  // {
-  //   path: 'school-3',
-  //   title: '校园3',
-  // },
-  // {
-  //   path: 'school-4',
-  //   title: '校园4',
-  // },
 ];
+
+// export const appRoutes = [
+//   { path: '', redirectTo: '/login/login-form', pathMatch: 'full' },
+//   { path: 'blank', loadChildren: () => import('./layout/blank/blank-routing') },
+//   { path: 'login', data: { preload: true }, loadChildren: () => import('./pages/login/login-routing') },
+//   { path: 'default', data: { preload: true }, loadChildren: () => import('./layout/default/default-routing') },
+//   { path: '**', redirectTo: '/login/login-form' }
+// ] as Routes;
