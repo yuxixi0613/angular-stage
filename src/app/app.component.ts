068ff446ceff4544606e2dd983ac68e0404c5e29
kiko <446094999@qq.com>
2025-10-23 02:49:52 +0800

    20

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
 
-import { RouterOutlet } from '@angular/router';
 import { DefaultComponent } from './layout/default/default.component';
 
 @Component({
-    selector: 'app-root',
-    imports: [RouterOutlet, DefaultComponent],
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.scss'
+  selector: 'app-root',
+  imports: [DefaultComponent],
+  template: `<app-layout-default></app-layout-default>`
 })
 export class AppComponent {
   title = 'Angular Stage';
