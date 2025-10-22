de737886f51d0e457f7f3bc6af29f8c46c098c93
kiko <446094999@qq.com>
2025-10-23 01:41:20 +0800

    add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { SelectCoordinateComponent } from './components/select-coordinate/select-coordinate.component';
 import { DefaultComponent } from './layout/default/default.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    CommonModule,
-    RouterOutlet,
-    MatSlideToggleModule,
-    SelectCoordinateComponent,
-    DefaultComponent
-  ],
+  imports: [CommonModule, RouterOutlet, DefaultComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
