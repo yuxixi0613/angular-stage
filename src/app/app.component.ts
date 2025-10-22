5aff4c3f3b94b933a8e380f25a91fa281f7a399b
kiko <446094999@qq.com>
2025-10-23 02:39:20 +0800

    20

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+
 import { RouterOutlet } from '@angular/router';
 import { DefaultComponent } from './layout/default/default.component';
 
 @Component({
     selector: 'app-root',
-    imports: [CommonModule, RouterOutlet, DefaultComponent],
+    imports: [RouterOutlet, DefaultComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
 })
