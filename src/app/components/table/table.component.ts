7549e3994c036c55190889d6c9a745088c789c30
kiko <446094999@qq.com>
2025-10-23 22:13:55 +0800

    add

---
+++ src/app/components/table/table.component.ts
@@ -0,0 +1,11 @@
+import { Component, type OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-table',
+  imports: [],
+  templateUrl: './table.component.html',
+  styleUrl: './table.component.scss'
+})
+export class TableComponent implements OnInit {
+  ngOnInit(): void {}
+}
